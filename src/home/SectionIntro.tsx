5dac31ff3046de5ff83079b362765555e0394bc7
bereal1995 <gygud98@gmail.com>
2023-07-12 00:24:41 +0900

    feat: 텍스트 애니메이션 추가

--- src/home/SectionIntro.tsx
+++ src/home/SectionIntro.tsx
@@ -1,4 +1,7 @@
-import { motion } from 'framer-motion'
+import { useRef } from 'react'
+import React from 'react'
+
+import { motion, useInView, Variants } from 'framer-motion'
 
 import { ISectionIntroContent } from '@/home/constants'
 
@@ -8,6 +11,31 @@ interface Props {
 
 export default function SectionIntro({ content }: Props) {
   const { title, description } = content
+  const ref = useRef<HTMLDivElement>(null)
+  const isInView = useInView(ref, { once: true })
+
+  const sentence: Variants = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+        staggerChildren: 0.1,
+      },
+    },
+  }
+
+  const letter: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  }
+
   return (
     <div className="pl-[60px] pr-[100px]">
       <motion.div
@@ -36,10 +64,57 @@ export default function SectionIntro({ content }: Props) {
             {title.sub}
           </span>
         </h2>
-        <div className="flex flex-col gap-[20px]">
-          {description.map((desc) => (
-            <p key={desc}>{desc}</p>
-          ))}
+        <div className="flex flex-col gap-[20px]" ref={ref}>
+          <motion.p
+            variants={sentence}
+            initial="hidden"
+            animate={isInView ? 'visible' : 'hidden'}
+          >
+            {description
+              .join(' \n\n ')
+              .split(' ')
+              .map((char, index) => {
+                const isNewLine = char === '\n\n'
+
+                if (isNewLine) {
+                  return (
+                    <React.Fragment key={`${char}-${index}`}>
+                      <br />
+                      <br />
+                    </React.Fragment>
+                  )
+                }
+
+                return (
+                  <motion.span
+                    key={`${char}-${index}`}
+                    variants={letter}
+                    className="inline-block"
+                  >
+                    {char}&nbsp;
+                  </motion.span>
+                )
+              })}
+          </motion.p>
+          {/* {description.map((desc) => (
+            <motion.p
+              key={desc}
+              variants={sentence}
+              initial="hidden"
+              animate={isInView ? 'visible' : 'hidden'}
+            >
+              {desc.split(' ').map((char, index) => (
+                <motion.span
+                  key={`${char}-${index}`}
+                  variants={letter}
+                  className="inline-block"
+                >
+                  {char}&nbsp;
+                </motion.span>
+              ))}
+            </motion.p>
+          ))} */}
+          {/* <div className="whitespace-pre-wrap">{description.join('\n\n')}</div> */}
         </div>
       </div>
     </div>
